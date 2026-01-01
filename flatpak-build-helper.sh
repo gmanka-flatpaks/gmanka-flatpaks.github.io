b575d9351e391cbd0ff5007aaea6e5579b4096a7
gmanka <gmankab@gmail.com>
2026-01-02 02:49:22 +0500

    [+] set branch

--- flatpak-build-helper.sh
+++ flatpak-build-helper.sh
@@ -6,5 +6,4 @@ which flatpak
 which flatpak-builder
 
 flatpak-builder --disable-rofiles-fuse --install-deps-from=flathub --repo=repo build $flatpak_id/$flatpak_id.yml
-flatpak build-bundle repo $flatpak_name-$runner_arch.flatpak $flatpak_id
-
+flatpak build-bundle repo $flatpak_name-$runner_arch.flatpak $flatpak_id $branch
